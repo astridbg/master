01ec0c77c606e6c21ccf00c251ee4f5dc2cab04c
Astrid Gjelsvik <astridbg@login3-nird-tos.nird.login.svc.cluster.local>
2022-02-28 13:30:44 +0100

    updated dataset units settings and decimal points settings in plots

--- model/scripts/getVars.py
+++ model/scripts/getVars.py
@@ -6,9 +6,9 @@ import glob
 from functions import *
 
 rpath="/projects/NS9600K/astridbg/noresm/cases/"
-#case = "def_20210126"
+case = "def_20210126"
 #case = "meyers92_20220210"
-case = "andenes21_20220222"
+#case = "andenes21_20220222"
 casefolder="NF2000climo_f19_tn14_"+case
 
 all_files = glob.glob(rpath+casefolder+"/atm/hist/"+casefolder+".cam.h0.*")
@@ -24,8 +24,25 @@ date = "2007-01-15_2010-03-15"
 
 wpath="/projects/NS9600K/astridbg/model_data/"
 
-variables = ["NIMEY","AWNI", "FREQI","CLDICE","SWCF","LWCF","CLDTOT","CLDHGH","CLDMED","CLDLOW","TGCLDIWP","TGCLDLWP","TREFHT"]
+#variables = ["NIMEY","AWNI", "FREQI","CLDICE","SWCF","LWCF","CLDTOT","CLDHGH","CLDMED","CLDLOW","TGCLDIWP","TGCLDLWP","TREFHT"]
 #variables = ["AWNI", "FREQI","CLDICE","SWCF","LWCF","CLDTOT","CLDHGH","CLDMED","CLDLOW","TGCLDIWP","TGCLDLWP","TREFHT"]
+variables = ["CLDICE","TREFHT"]
 for var in variables:
 	print("Started writing:",var)
+
+	if var == "NIMEY":
+		ds[var].values = ds[var].values*1e-3 # Change unit to number per litre
+		ds[var].attrs["units"] = "1/L"
+	
+	if var == "TREFHT":
+		ds[var].values = ds[var].values - 273.15 # Change unit to degrees Celsius
+		ds[var].attrs["units"] = r"$^{\circ}$C"
+
+	if var == "CLDICE": 
+		ds[var].values = ds[var].values*1e+3 # Change unit to grams per kilograms
+		ds[var].attrs["units"] = "g/kg"
+	print(ds[var].attrs["units"])
+	
 	ds[var].to_netcdf(wpath+var+"_"+case+"_"+date+".nc")
+
+
--- model/scripts/plottest.py
+++ model/scripts/plottest.py
@@ -7,29 +7,29 @@ import cartopy.crs as ccrs
 rpath="/projects/NS9600K/astridbg/model_data/"
 
 #case = "def_20210126"
-case = "meyers92_20220210" 				
-#case = "andenes21_20220222"
+#case = "meyers92_20220210" 				
+case = "andenes21_20220222"
 timelength = "2007-01-15_2010-03-15"
 
-var = "CLDICE"
+var = "TREFHT"
 
 ds = xr.open_dataset(rpath+var+"_"+case+"_"+timelength+".nc")
 
-#timepoint = 3
-#date = str(ds.time[timepoint].values).split(" ")[0]
-ds = ds.sel(lev="750", method="nearest").mean("time")
+timepoint = 3
+date = str(ds.time[timepoint].values).split(" ")[0]
+#ds = ds.sel(lev="750", method="nearest").mean("time")
 
 fig = plt.figure(1, figsize=[10,5])
 
 ax = plt.subplot(1, 1, 1, projection=ccrs.PlateCarree())
 
-#ds[var].isel(time=timepoint).plot(ax=ax, transform=ccrs.PlateCarree(), cmap='viridis')
-ds[var].plot(ax=ax, transform=ccrs.PlateCarree(), cmap='viridis')
+ds[var].isel(time=timepoint).plot(ax=ax, transform=ccrs.PlateCarree(), cmap='viridis')
+#ds[var].plot(ax=ax, transform=ccrs.PlateCarree(), cmap='viridis')
 
-#plt.title(var+" "+date+" "+case, fontsize=18)
-plt.title(var+" time mean "+case, fontsize=18)
+plt.title(var+" "+date+" "+case, fontsize=18)
+#plt.title(var+" time mean "+case, fontsize=18)
 
 
 ax.coastlines()
 
-plt.savefig("../figures/"+var+"_"+case+"_test.png")
+plt.savefig("../figures/test/"+var+"_"+case+"_test.png")
